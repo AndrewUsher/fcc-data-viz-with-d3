0997b554a22e3737cf6e7105b1e572f5e65c26d5
Andrew Usher <andrewusher00@gmail.com>
2019-02-05 09:22:34 -0600

    Complete lesson 6

---
+++ pages/6-change-styles-based-on-data/index.js
@@ -0,0 +1,11 @@
+import * as d3 from 'd3'
+
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9]
+
+d3.select('body')
+  .selectAll('h2')
+  .data(dataset)
+  .enter()
+  .append('h2')
+  .text(d => `${d} USD`)
+  .style('color', d => (d < 20 ? 'red' : 'green'))
